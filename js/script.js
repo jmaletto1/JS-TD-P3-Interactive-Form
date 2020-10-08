059bf739da489dd25550d3090268a625a55c92bf
Johnny Austen <65448382+jmaletto1@users.noreply.github.com>
2020-10-08 19:41:35 +0100

    First Set of Files

---
+++ js/script.js
@@ -0,0 +1,70 @@
+function selectFocus(element) {
+	document.getElementById(element).focus();
+}
+
+//Call the selectFocus function, which sets the User's input to the Name input.
+selectFocus("name");
+
+//Hides the "Your Job Role" (other) field and label
+$('#other-title').hide();
+$('#other-title-label').hide();
+
+//T-Shirt Info Function
+
+const $tColor = $('#color');
+const $themeDefault = `<option value="default">Select Colour to View Options</option>`;
+$tColor.prepend($themeDefault);
+$tColor.val("default");
+
+//Hide Colours
+
+function hideColours() {
+$tColor.children('option[value="cornflowerblue"]').css('display','none');
+$tColor.children('option[value="darkslategrey"]').css('display','none');
+$tColor.children('option[value="gold"]').css('display','none');
+$tColor.children('option[value="tomato"]').css('display','none');
+$tColor.children('option[value="steelblue"]').css('display','none');
+$tColor.children('option[value="dimgrey"]').css('display','none');
+$tColor.val("default");
+}
+
+// hideColours();
+
+$tDesign = $('#design');
+$tDesign.on('change', function() {
+if ($tDesign.val() === "js puns") {
+$tColor.children('option[value="cornflowerblue"]').css('display','block');
+$tColor.children('option[value="darkslategrey"]').css('display','block');
+$tColor.children('option[value="gold"]').css('display','block');
+$tColor.children('option[value="tomato"]').css('display','none');
+$tColor.children('option[value="steelblue"]').css('display','none');
+$tColor.children('option[value="dimgrey"]').css('display','none');
+$tColor.children('option[value="default"]').css('display','none');
+$tColor.val("cornflowerblue");
+}
+else if ($tDesign.val() === "heart js") {
+$tColor.children('option[value="cornflowerblue"]').css('display','none');
+$tColor.children('option[value="darkslategrey"]').css('display','none');
+$tColor.children('option[value="gold"]').css('display','none');
+$tColor.children('option[value="tomato"]').css('display','block');
+$tColor.children('option[value="steelblue"]').css('display','block');
+$tColor.children('option[value="dimgrey"]').css('display','block');
+$tColor.children('option[value="default"]').css('display','none');
+$tColor.val("tomato");
+}
+else if ($tDesign.val() !== "js puns" || $tDesign.val() !== "heart js") {
+hideColours();
+}
+}); 
+
+
+
+
+
+
+// function tShirtSelector() {
+
+// }
+
+
+
